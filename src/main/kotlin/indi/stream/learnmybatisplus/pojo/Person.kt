90743d4874e7de90ce570872979b6f8ab7941fea
Stream <1542763342@qq.com>
2024-08-10 07:21:49 +0800

    feat: Person data class

--- src/main/kotlin/indi/stream/learnmybatisplus/pojo/Person.kt
+++ src/main/kotlin/indi/stream/learnmybatisplus/pojo/Person.kt
@@ -1,7 +1,9 @@
 package indi.stream.learnmybatisplus.pojo
 
+import com.baomidou.mybatisplus.annotation.TableName
 import com.baomidou.mybatisplus.extension.activerecord.Model
 
+@TableName("persons")
 class Person(
     var name: String,
     var age: Int,
