6602105cdb82d7701e7420c80e6a3cd4b76f867b
Stream <1542763342@qq.com>
2024-08-10 06:56:19 +0800

    init

---
+++ src/main/kotlin/indi/stream/learnmybatisplus/LearnMyBatisPlusApplication.kt
@@ -0,0 +1,11 @@
+package indi.stream.learnmybatisplus
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LearnMyBatisPlusApplication
+
+fun main(args: Array<String>) {
+    runApplication<LearnMyBatisPlusApplication>(*args)
+}
---
+++ src/test/kotlin/indi/stream/learnmybatisplus/LearnMyBatisPlusApplicationTests.kt
@@ -0,0 +1,13 @@
+package indi.stream.learnmybatisplus
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LearnMyBatisPlusApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
